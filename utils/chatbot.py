667d87d428fa1431204bb80cd2a9b938400be041
ro-goncalves <ro.go.calves@gmail.com>
2024-03-26 15:04:56 -0300

    Tentando criar um prompt template

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -5,6 +5,7 @@ from langchain.chat_models.openai import ChatOpenAI
 from langchain.memory import ConversationBufferMemory, ConversationSummaryMemory
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain_community.llms import HuggingFaceEndpoint
+from langchain.prompts.prompt import PromptTemplate
 
 
 def create_vectorstore(chunks):
@@ -13,15 +14,21 @@ def create_vectorstore(chunks):
     WhereIsAI/UAE-Large-V1
     """
     # embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(       
+    #     model_name='ricardo-filho/bert-base-portuguese-cased-nli-assin-2'
+    # )
+    # new_vectorstrore = FAISS.from_texts(texts=chunks, embedding=embeddings)
+    
+    # vectorstrore = FAISS.load_local('faiss_default', embeddings)
+    # vectorstrore.merge_from(new_vectorstrore)
+
     embeddings = HuggingFaceInstructEmbeddings(       
         model_name='ricardo-filho/bert-base-portuguese-cased-nli-assin-2'
     )
-    new_vectorstrore = FAISS.from_texts(texts=chunks, embedding=embeddings)
+    vactorstrore = FAISS.from_texts(texts=chunks, embedding=embeddings)
     
-    vectorstrore = FAISS.load_local('faiss_default', embeddings)
-    vectorstrore.merge_from(new_vectorstrore)
-    
-    return vectorstrore
+    return vactorstrore
+
 
 def create_conversation_chain(vectorstore=None):
     """
@@ -29,7 +36,7 @@ def create_conversation_chain(vectorstore=None):
     meta-llama/Llama-2-7b-chat-hf
     mistralai/Mixtral-8x7B-Instruct-v0.1
     """
-    # llm = ChatOpenAI()
+    # llm = ChatOpenAI()   
     
     if not vectorstore:
         embeddings = HuggingFaceInstructEmbeddings(       
@@ -37,15 +44,27 @@ def create_conversation_chain(vectorstore=None):
         )
         vectorstore = FAISS.load_local('faiss_default', embeddings)
     
+    template = """
+        Você é um robô chamado Brian, seu objetivo é responder perguntas sobre consórcio de um forma cordial.
+        Seja muito respeitoso e sempre use um português polido.
+
+        Conversa atual:
+              
+        _bot:
+    """
+
+    PROMPT = PromptTemplate(input_variables=None, template=template)
+
     llm = HuggingFaceEndpoint(
         repo_id='mistralai/Mixtral-8x7B-Instruct-v0.1',
-        huggingfacehub_api_token='', 
+        huggingfacehub_api_token='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', 
         temperature=0.5,
         model_kwargs={'max_length':512})
     
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     
     conversation_chain = ConversationalRetrievalChain.from_llm(
+        combine_docs_chain_kwargs={'prompt': PROMPT},
         llm=llm,
         retriever=vectorstore.as_retriever(),
         memory=memory
